b24b5dec22c83cfa24e438a7f099e873e3d3247b
Atuldubey98 <32754884+Atuldubey98@users.noreply.github.com>
2020-06-22 20:36:46 +0530

    commit on 'master'

--- lib/widget/chatbuilder.dart
+++ lib/widget/chatbuilder.dart
@@ -1,81 +1,136 @@
-import 'dart:async';
-
-import 'package:createTask/provider/messagePro.dart';
+import 'package:flutter_socket_io/socket_io_manager.dart';
+import 'package:flutter_socket_io/flutter_socket_io.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 
-import 'package:provider/provider.dart';
-
 class ChatBuilder extends StatefulWidget {
-  final String receiver;
+  
   final String username;
-  ChatBuilder(this.receiver, this.username);
+  ChatBuilder(this.username);
 
   @override
   _ChatBuilderState createState() => _ChatBuilderState();
 }
 
 class _ChatBuilderState extends State<ChatBuilder> {
-  Timer _time;
+  List<String> messages;
+  double height;
+  String socketUrl = "http://192.168.0.111:5000";
+  double width;
+  SocketIO socketIO;
+  final textController = TextEditingController();
+  
+ 
+    
+  Widget buildMessageList() {
+    return Container(
+      height: height * 0.8,
+      width: width,
+      child: ListView.builder(
+        itemCount: messages.length,
+        itemBuilder: (BuildContext context, int index) {
+          return buildSingleMessage(index);
+        },
+      ),
+    );
+  }
+  Widget buildSingleMessage(int index) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        margin: const EdgeInsets.only(bottom: 20.0, left: 20.0),
+        decoration: BoxDecoration(
+          color: Colors.deepPurple,
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: Text(
+          messages[index],
+          style: TextStyle(color: Colors.white, fontSize: 15.0),
+        ),
+      ),
+    );
+  }
 
-  @override
-  void didChangeDependencies() {
-    _time = new Timer.periodic(Duration(milliseconds: 100), (_) {
-      Provider.of<MessagePro>(context, listen: false)
-          .fetchPost(widget.receiver, widget.username);
+  Widget buildInputArea() {
+    return Container(
+      height: height * 0.1,
+      width: width,
+      child: Row(
+        children: <Widget>[
+          buildChatInput(),
+          buildSendButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildSendButton() {
+    return FloatingActionButton(
+      backgroundColor: Colors.deepPurple,
+      onPressed: () {
+        //Check if the textfield has text or not
+        if (textController.text.isNotEmpty) {
+          //Send the message as JSON data to send_message event
+          socketIO.sendMessage(
+              'message', json.encode({'message': textController.text}));
+          //Add the message to the list
+          this.setState(() => messages.add(textController.text));
+          textController.text = '';
+          //Scrolldown the list to show the latest message
+
+        }
+      },
+      child: Icon(
+        Icons.send,
+        size: 30,
+      ),
+    );
+  }
+
+  Widget buildChatInput() {
+    return Container(
+      width: width * 0.7,
+      padding: const EdgeInsets.all(2.0),
+      margin: const EdgeInsets.only(left: 40.0),
+      child: TextField(
+        decoration: InputDecoration.collapsed(
+          hintText: 'Send a message...',
+        ),
+        controller: textController,
+      ),
+    );
+  }
+@override
+  void initState() {
+       messages = List<String>();
+     SocketIO socketIO = SocketIOManager().createSocketIO(
+      socketUrl,
+      '/',
+      
+    );
+    socketIO.init();
+    socketIO.connect();
+   
+    socketIO.subscribe("recmessage", (jsonData) {
+      Map<String, dynamic> data = json.decode(jsonData);
+      this.setState(() => messages.add(data['message']));
     });
-
-    super.didChangeDependencies();
+    
+    super.initState();
   }
-
-  @override
-  void dispose() {
-    _time.cancel();
-
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Consumer<MessagePro>(
-      child: Center(
-        child: Text('Your are Lonely'),
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          SizedBox(height: height * 0.1),
+          buildMessageList(),
+          buildInputArea()
+        ],
       ),
-      builder: (context, message, child) => message.list.length <= 0
-          ? child
-          : ListView.builder(
-              itemCount: message.list.length,
-              itemBuilder: (context, index) {
-                return Column(
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        CircleAvatar(),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.05,
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.greenAccent,
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(8),
-                            ),
-                          ),
-                          width: MediaQuery.of(context).size.width * 0.7,
-                          alignment: Alignment.topLeft,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              message.list[index].message,
-                              style: TextStyle(fontSize: 20),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Divider()
-                  ],
-                );
-              }),
     );
   }
 }
